9861a11a8012a0404f1ec0729f443f4b88e66542
BartoszHab <113014007+BartoszHab@users.noreply.github.com>
2024-09-23 16:44:59 +0200

    Add files via upload

---
+++ indeks.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Skrypt PHP</title>
+    <meta charset="UTF-8">
+
+</head>
+<body>
+<p>
+<?php
+//$x = 1;
+//$nazwa_1 = "tekst";
+//$liczba7 = 345;
+//$ilość = 4;
+//echo "Wynik wynosi $x <br>";
+//echo "To jest $nazwa_1 1 <br>";
+//echo "$liczba7 <br>";
+//echo "bieżąca wartość to $ilość!";
+//<br>
+//$imie = "Anna";
+//$jezyk1 = "PHP";
+//$jezyk2 = "javascript";
+//$tekst = <<< TX
+//Mam na imie $imie.
+//Ucze sie programowac w jezyku $jezyk1.
+//Umiem juz programowac w $jezyk2.
+//TX;
+//echo $tekst;
+//$dane = array(
+//    array("nazwisko" => "kowalski", "imie" =>" Jan", "wiek" => "27"),
+//    array("nazwisko" => "nowak", "imie" =>" Pawel", "wiek" => "24"),
+ //   array("nazwisko" => "Gorka", "imie" =>" Tomasz", "wiek" => "29"),
+//)
+
+$liczbaCalkowita = 10;
+$liczbaRzeczywista = 5.75;
+$ciagZnakow = "Witaj";
+
+echo "wartosz liczby calkowitej to $liczbaCalkowita"<br>;
+echo "wartosz liczby rzeczywistej to $liczbaRzeczywista"<br>;
+echo "wartosz ciagu znaków to $ciagZnakow"<br>;
+?>
+</p>
+</body>
+</html>
